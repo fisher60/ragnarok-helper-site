ea132426341233aa8b04fa8bf82ae1d977c97f6e
GDWR <gregory.dwr@gmail.com>
2021-12-29 01:59:37 +0000

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+// import MapSvg from './assets/map.svg';
+import { Map } from './components/Map';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Map />
+    </div>
+  )
+}
+
+export default App
