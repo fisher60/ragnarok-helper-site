bd6953325b61aa049d7fa61066c06b88fdbbce8f
fisher60 <wakefollower@gmail.com>
2022-01-09 13:50:32 -0600

    tailwind half working

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    colors: {
-      'primary': '#4F86C6',
-      'secondary': '#B37BA4',
-      'tertiary': '#1C3041',
-      'pale-blue': '#DCEDFF',
-      'light-blue': '#2EC4B6',
-    },
+    // colors: {
+    //   'primary': '#4F86C6',
+    //   'secondary': '#B37BA4',
+    //   'tertiary': '#1C3041',
+    //   'pale-blue': '#DCEDFF',
+    //   'light-blue': '#2EC4B6',
+    // },
     extend: {},
   },
   plugins: [],
